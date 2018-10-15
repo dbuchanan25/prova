55d905d940c6473ed9fe7d01ee49ef75f8925b55
dbuchanan25 <40598513+dbuchanan25@users.noreply.github.com>
2018-10-15 10:36:33 -0400

    Update blockInformation.php

--- blockInformation.php
+++ blockInformation.php
@@ -237,6 +237,7 @@ if ($_SESSION['ptblock1'] && !$_SESSION['ptblock2'])
   <a href="ptblock1.php">Postoperative Day #1</a>
   <a href="blockInformation.php">Block Information</a>
   <a href="faq.php">FAQs</a>
+  <a href="logout.php">Logout</a>
 </div>
     
 <?php
@@ -250,6 +251,7 @@ else if ($_SESSION['ptblock1'] && $_SESSION['ptblock2'])
   <a href="ptblock2.php">Postoperative Day #2</a>
   <a href="blockInformation.php">Block Information</a>
   <a href="faq.php">FAQs</a>
+  <a href="logout.php">Logout</a>
 </div>
 <?php
 }
@@ -260,6 +262,7 @@ else if (!($_SESSION['ptblock1'] && !$_SESSION['ptblock2']))
   <a href="eos.php">Evening of Surgery</a>
   <a href="blockInformation.php">Block Information</a>
   <a href="faq.php">FAQs</a>
+  <a href="logout.php">Logout</a>
 </div>
 <?php
 }
