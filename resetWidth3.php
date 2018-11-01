4af9faddd33550dc23be2cb41c60444fd7a687e9
dbuchanan25 <40598513+dbuchanan25@users.noreply.github.com>
2018-10-31 23:33:03 -0400

    Update resetWidth3.php

--- resetWidth3.php
+++ resetWidth3.php
@@ -1,11 +1,23 @@
 <?php
 session_start();
-    if (isset($_GET["w"]))
+if (!isset($_SESSION['username']))
+{
+    require_once ('includes/login_functions.inc.php');
+    $url = absolute_url();
+    header("Location: $url");
+    exit();
+}
+else
+{
+    if (isset($_GET['w']))
     {
         $_SESSION['w'] = $_GET['w'];
     }
+    ?>
+    <script type="text/javascript">
+        window.location.assign = (history.go(-1));
+    </script>
+    <?php
+}
 ?>
-<script type="text/javascript">
-    window.location.assign = (history.go(-1));
-</script>
 
